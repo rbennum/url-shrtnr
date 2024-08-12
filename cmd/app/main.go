b0962966777c42d625ff25aeeb454ba5b80e9758
Bening Ranum <8705838+rbennum@users.noreply.github.com>
2024-08-12 14:20:38 +0700

    feat: add basic db configuration

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rbennum/url-shrtnr/config"
+	"github.com/rbennum/url-shrtnr/db"
 )
 
 func main() {
+	// load config from .env file
 	config.LoadConfig()
-	// TODO: establish DB connection
 
-	// gin_handler := gin.Default()
+	// initiate DB connection
+	connectDB()
 
-	// serv := createServer("", gin_handler)
+	gin_handler := gin.Default()
 
+	main_addr := config.GetEnv("ADDR_ROUTE", "localhost")
+	main_port := config.GetEnv("PORT", "8080")
+	short_addr := config.GetEnv("ADDR_ROUTE_SHORTEN", "localhost")
+	short_port := config.GetEnv("PORT_SHORTEN", "8088")
+	main_serv := createServer(main_addr + ":" + main_port, gin_handler)
+	short_serv := createServer(short_addr + ":" + short_port, gin_handler)
+
+	// initiate shutdown if triggered
+	initiateShutdown(main_serv)
+	initiateShutdown(short_serv)
 }
 
 func createServer(addr string, handler *gin.Engine) *http.Server {
@@ -23,14 +41,44 @@ func createServer(addr string, handler *gin.Engine) *http.Server {
 		Addr: addr,
 		Handler: handler,
 	}
-	
 	go func() {
-		log.Printf("Listening to %s:%s...", "addr_route", "port")
+		log.Printf("Listening to %s...", addr)
 		if err := serv.ListenAndServe(); 
 				err != nil && err != http.ErrServerClosed {
 				log.Fatalf("Listen: %s\n", err)
 		}
 	}()
-
 	return serv
+}
+
+func connectDB() {
+	port, errconv := strconv.Atoi(config.GetEnv("DB_PORT", "")) 
+	if errconv != nil {
+		panic(errconv)
+	}
+	opts := db.PoolOptions {
+		Host: config.GetEnv("DB_HOST", ""),
+		Port: port,
+		User: config.GetEnv("DB_USER", ""),
+		DBName: config.GetEnv("DB_NAME", ""),
+		Pass: config.GetEnv("DB_PASS", ""),
+	}
+	errdb := db.Init(opts)
+	if errdb != nil {
+		panic(errdb)
+	}
+}
+
+func initiateShutdown(serv *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<- quit
+	log.Println("Shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := serv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Printf("Server %s exiting...", serv.Addr)
+	db.Pool_DB.Close()
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/rbennum/url-shrtnr
 
 go 1.22.2
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	github.com/bytedance/sonic v1.12.1 // indirect
@@ -15,7 +20,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
